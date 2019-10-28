23b0c57c175db84b24980c41b4bf8a9aa216b6eb
jiglionero <smouk.chayz@gmail.com>
2019-10-28 17:52:04 +0300

    parfenov 3

--- src/Main.java
+++ src/Main.java
@@ -29,15 +29,17 @@ public class Main extends Application {
     RadioButton beta;
     RadioButton gamma;
     RadioButton delta;
-    NumberTextField boundsCount;
-    NumberTextField a;
-    NumberTextField b;
+    NumberTextField alphaField;
+    NumberTextField betaField;
+    NumberTextField gammaField;
+    NumberTextField deltaField;
+    NumberTextField p;
+    NumberTextField n;
+    NumberTextField m;
     LineChart<Number, Number> chart;
     HBox root;
 
-    CheckBox F;
-    CheckBox f;
-    int prevT;
+    NumberTextField x0;
 
     NumberTextField step;
     Label integral;
@@ -73,10 +75,6 @@ public class Main extends Application {
         C = new NumberTextField(2);
         Label bottom = new Label("Bottom:");
         D = new NumberTextField(-2);
-        Label aL = new Label("a:");
-        a = new NumberTextField(-2);
-        Label bL = new Label("b:");
-        b = new NumberTextField(2);
         wGrid.add(left, 0, 0);
         wGrid.add(A, 1, 0);
         wGrid.add(right, 0, 1);
@@ -85,10 +83,6 @@ public class Main extends Application {
         wGrid.add(D, 1, 2);
         wGrid.add(top, 0, 3);
         wGrid.add(B, 1, 3);
-        wGrid.add(aL, 0, 4);
-        wGrid.add(a, 1, 4);
-        wGrid.add(bL, 0, 5);
-        wGrid.add(b, 1, 5);
 
         Label fParams = new Label("Function parameters");
 
@@ -97,20 +91,32 @@ public class Main extends Application {
         fGrid.setVgap(10);
         Label alphaL = new Label("Alpha:");
         alpha = new RadioButton();
+        alphaField = new NumberTextField(1);
         Label betaL = new Label("Beta:");
         beta = new RadioButton();
+        betaField = new NumberTextField(1);
         Label gammaL = new Label("Gamma:");
         gamma = new RadioButton();
+        gammaField = new NumberTextField(1);
         Label deltaL = new Label("Delta:");
         delta = new RadioButton();
+        deltaField = new NumberTextField(1);
+        x0 = new NumberTextField(0);
+        Label x0L = new Label("x0:");
         fGrid.add(alphaL, 0, 0);
         fGrid.add(alpha, 1, 0);
+        fGrid.add(alphaField, 2, 0);
         fGrid.add(betaL, 0, 1);
         fGrid.add(beta, 1, 1);
+        fGrid.add(betaField, 2, 1);
         fGrid.add(gammaL, 0, 2);
         fGrid.add(gamma, 1, 2);
+        fGrid.add(gammaField, 2, 2);
         fGrid.add(deltaL, 0, 3);
         fGrid.add(delta, 1, 3);
+        fGrid.add(deltaField, 2, 3);
+        fGrid.add(x0L, 0, 4);
+        fGrid.add(x0, 1, 4);
 
         ToggleGroup group = new ToggleGroup();
         alpha.setToggleGroup(group);
@@ -122,26 +128,24 @@ public class Main extends Application {
         GridPane bGrid = new GridPane();
         bGrid.setHgap(10);
         bGrid.setVgap(10);
-        Label nBoundL = new Label("Count of integration bounds:");
-        boundsCount = new NumberTextField(500);
-        bGrid.add(nBoundL, 0, 0);
-        bGrid.add(boundsCount, 1, 0);
-
-        HBox functions = new HBox();
-        functions.setSpacing(10);
-        f = new CheckBox("f(x)");
-        f.setSelected(true);
-        F = new CheckBox("F(x)");
-        F.setSelected(true);
-        functions.getChildren().addAll(f, F);
+        Label pBoundL = new Label("p:");
+        p = new NumberTextField(1);
+        Label nBoundL = new Label("n:");
+        n = new NumberTextField(500);
+        Label mBoundL = new Label("m:");
+        m = new NumberTextField(200);
+        bGrid.add(pBoundL, 0, 0);
+        bGrid.add(p, 1, 0);
+        bGrid.add(nBoundL, 0, 1);
+        bGrid.add(n, 1, 1);
+        bGrid.add(mBoundL, 0, 2);
+        bGrid.add(m, 1, 2);
 
         GridPane res = new GridPane();
-        step = new NumberTextField(0.01);
+        step = new NumberTextField(0.1);
         integral = new Label();
         res.add(new Label("Step = "),0,0);
         res.add(step,1,0);
-        res.add(new Label("I = "),0,1);
-        res.add(integral,1,1);
 
 
         Scene scene = new Scene(root, 1300, 700);
@@ -154,10 +158,10 @@ public class Main extends Application {
             }
         });
 
-        Label name = new Label("ИВТ-32БО Бондарь Алеся.");
+        Label name = new Label("ИВТ-42БО Суендуков Михаил.");
         name.setAlignment(Pos.BOTTOM_LEFT);
 
-        leftPane.getChildren().addAll(wParams, wGrid, fParams, fGrid, bGrid, functions, res, commit,name);
+        leftPane.getChildren().addAll(wParams, wGrid, fParams, fGrid, bGrid, res, commit, name);
 
         root.getChildren().add(leftPane);
 
@@ -167,15 +171,13 @@ public class Main extends Application {
     }
 
     public void initPlot() {
-
-        integral.setText("0");
         Function f = new Function() {
             @Override
-            public double getValueAt(double x) {
-                double a = 1;
-                double b = 1;
-                double g = 1;
-                double d = 1;
+            public double getValueAt(double x, double y) {
+                double a = alphaField.getNumber();
+                double b = betaField.getNumber();
+                double g = gammaField.getNumber();
+                double d = deltaField.getNumber();
                 if(alpha.isSelected()){
                     a = x;
                 }
@@ -189,34 +191,7 @@ public class Main extends Application {
                     d = x;
                 }
 
-                return a * (Math.sin(b / ((x - g) * (x - g)))) * Math.cos(d * 1);
-                //return Math.sin(x);
-            }
-        };
-        double h = ((double) (C.getNumber() - A.getNumber()))/(boundsCount.getNumber()-1);
-        Function F = new Function() {
-            @Override
-            public double getValueAt(double x) {
-                double start = A.getNumber();
-                int t = (int) ((x - start)/h);
-                double value1 = f.getValueAt(start + (t-1)*h);
-                double value2 = f.getValueAt(start + (t-0.5)*h);
-                double value3 = f.getValueAt(start + (t)*h);
-                if (Double.isNaN(value1)){
-                    value1 = Double.MIN_VALUE;
-                }
-                if (Double.isNaN(value2)){
-                    value2 = Double.MIN_VALUE;
-                }
-                if (Double.isNaN(value3)){
-                    value3 = Double.MIN_VALUE;
-                }
-                double tmp = h/6*(value1+4*value2+value3);
-                if (prevT!=t) {
-                    integral.setText(String.valueOf(Double.parseDouble(integral.getText()) + tmp));
-                }
-                prevT = t;
-                return Double.parseDouble(integral.getText())+tmp;
+                return a * (Math.sin(b / ((y - g) * (y - g)))) * Math.cos(d * 1);
             }
         };
 
@@ -226,21 +201,31 @@ public class Main extends Application {
 
         chart = new LineChart<Number, Number>(new NumberAxis(A.getNumber(), C.getNumber(),(C.getNumber()-A.getNumber())/10), new NumberAxis(D.getNumber(), B.getNumber(),(B.getNumber()-D.getNumber())/10));
         chart.setMinWidth(1000);
+        chart.setCreateSymbols(true);
         chart.getStylesheets().add(Main.class.getResource("res/ch.css").toExternalForm());
         root.getChildren().add(chart);
 
         double step = this.step.getNumber();
+        double x0 = this.x0.getNumber();
+        double xn = x0;
+        int m = (int) this.m.getNumber();
+        int n = (int) this.n.getNumber();
+        int p = (int) this.p.getNumber();
         ArrayList<ArrayList<Pair<Double, Double>>> data = new ArrayList<>();
-        data.add(new ArrayList<>());
-        data.add(new ArrayList<>());
-        if(this.f.isSelected()){
-            for (double j = A.getNumber(); j < C.getNumber(); j+=step) {
-                data.get(0).add(new Pair<>(j, f.getValueAt(j)));
-            }
-        }
-        if(this.F.isSelected()){
-            for (double j = a.getNumber(); j < b.getNumber(); j+=h) {
-                data.get(1).add(new Pair<>(j, F.getValueAt(j)));
+        ArrayList<Pair<Double, Double>> mainLine = new ArrayList<>();
+        ArrayList<Pair<Double, Double>> pLine = new ArrayList<>();
+        data.add(mainLine);
+        data.add(pLine);
+        for (double j = A.getNumber(); j < C.getNumber(); j+=step) {
+            xn = x0;
+            for(int i = 0;i<m+n;i++){
+                if(i>m){
+                    mainLine.add(new Pair<>(j, xn));
+                    if(i%p==0) {
+                        pLine.add(new Pair<>(j, xn));
+                    }
+                }
+                xn = f.getValueAt(j,xn);
             }
         }
 
--- src/functions/Function.java
+++ src/functions/Function.java
@@ -1,26 +1,6 @@
 package functions;
 
 public abstract class Function {
-
-    public abstract double getValueAt(double x);
-
-    public double getDerivativeAt(double x, double dx){
-        double df = getValueAt(x+dx/2)-getValueAt(x-dx/2);
-        return df/dx;
-    }
-
-    public double getIntegralAt(double x, double dx, IntegralStyle style){
-        switch (style){
-            case SQUAD:
-                return getValueAt(x)*dx;
-            case TRAPEZE:
-                return (getValueAt(x)+getValueAt(x-dx))/2 * dx;
-        }
-        return 0;
-    }
-
-    public enum IntegralStyle{
-        SQUAD, TRAPEZE
-    }
+    public abstract double getValueAt(double x, double y);
 
 }
